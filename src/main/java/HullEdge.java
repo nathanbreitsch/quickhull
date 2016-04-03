4181d079bbd970034e193b9999319436202ac66d
nathan breitsch <nathanbreitsch@gmail.com>
2016-04-02 23:12:31 -0400

    first commit

---
+++ src/main/java/HullEdge.java
@@ -0,0 +1,21 @@
+
+public class HullEdge{
+  public Point origin;
+  public Point terminus;
+  public boolean flag; //mark true if no points are to the right
+
+  public HullEdge(Point origin, Point terminus){
+    flag = false;
+    this.origin = origin;
+    this.terminus = terminus;
+  }
+
+  //returns number monotonic with positive distance to right of edge
+  //returns something negative if to left of edge
+  //in this case, we compute area of parallelagram made by origin terminus and p
+  public double metric(Point p){
+    Point v = Point.difference(this.terminus, this.origin);
+    Point w = Point.difference(p, this.terminus);
+    return Point.crossProduct(v, w);
+  }
+}
---
+++ src/main/java/Point.java
@@ -0,0 +1,18 @@
+public class Point{
+  public double x;
+  public double y;
+
+  public Point(double x, double y){
+    this.x = x;
+    this.y = y;
+  }
+
+  public static Point difference(Point p1, Point p2){
+    return new Point(p1.x - p2.x, p1.y - p2.y);
+  }
+
+  public static double crossProduct(Point p1, Point p2){
+    return p1.x * p2.y - p1.y * p2.x;
+  }
+
+}
